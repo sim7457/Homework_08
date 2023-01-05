490071c2014d42c3b867149bc116ad7a6eb1bc76
sim7457 <118698648+sim7457@users.noreply.github.com>
2023-01-05 17:48:38 +0900

    230105 17:48

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
 $(function () {
+    $('#header .flex_inner .gnb>ul>li').hover(function () {
+        var idx = $(this).parent().index();
+        $(this).addClass('on');
+
+    }, function () {
+        $(this).removeClass('on');
+    })
+
+    $('.main_slick .right .inner').slick({
+        autoplay: true,
+        autoplaySpeed: 1500,
 
 
-
+    })
 
 
 
